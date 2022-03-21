ed24bbb29e669acf74ab5f4bc47b39bf1f6293fe
Mario Günzel <mario@Air-von-Mario.fritz.box>
2022-03-21 15:47:33 +0100

    add base task set to cause-effect chain object

--- e2e/cechains/chain.py
+++ e2e/cechains/chain.py
@@ -3,7 +3,10 @@ from tasks.taskset import TaskSet
 
 class CEChain(TaskSet):
     """A cause-effect chain."""
-    pass
+
+    def __init__(self, *args, base_ts=None):
+        self.base_ts = base_ts  # base task set (needed for some analyses)
+        super().__init__(*args)
 
 
 if __name__ == '__main__':
