b0c54ad78779ff754794332849c390c4445bd571
Mario Günzel <mario@Air-von-Mario.fritz.box>
2022-03-21 15:47:52 +0100

    cutting theorem based analysis

--- e2e/analyses/our.py
+++ e2e/analyses/our.py
@@ -15,8 +15,27 @@ def _cut_chain(ch):
     return new_chains
 
 
-def sporadic():
-    pass
+def cut_based(chain, LET=None, impl=None):
+    """Our cutting theorem based analysis."""
+    base_analyses = {  # TODO add other analyses
+        'duerr': None,
+    }  # Base analyses that can be utilized for LET and implicit communication analysis
+
+    # cut chain
+    cutted_chain = _cut_chain(chain)
+
+    # perform analysis
+    result = 0
+    for ch in cutted_chain:
+        comm = ch.check_feature('comm')  # check communication
+        if comm == 'impl':
+            result += base_analyses[impl](ch)
+        elif comm == 'LET':
+            result += base_analyses[LET](ch)
+        else:
+            raise RuntimeError(f'The communication behavior {comm=} is not covered by the analysis.')
+
+    return result
 
 
 if __name__ == '__main__':
